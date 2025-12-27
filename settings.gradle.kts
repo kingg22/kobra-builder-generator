3f94e0306e9cccd3afe5590bd841d3fb6933bed6
Rey <89825425+kingg22@users.noreply.github.com>
2025-12-26 22:11:05 -0500

    build(gradle): add settings.gradle.kts with intellij platform

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.jetbrains.intellij.platform.settings") version "2.10.5"
+    // Use the Foojay Toolchains plugin to automatically download JDKs required by subprojects.
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
+
+    repositories {
+        mavenCentral()
+
+        intellijPlatform {
+            defaultRepositories()
+        }
+    }
+}
+
+
+rootProject.name = "Kobra-Builder"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
