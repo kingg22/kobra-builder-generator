ddd605697b3ffcf35585ee86f8003152f29b778f
Rey <89825425+kingg22@users.noreply.github.com>
2025-12-27 00:35:13 -0500

    perf: remove unnecessary factory of pico container components

--- src/main/java/pl/mjedynak/idea/plugins/builder/action/GoToBuilderAction.java
+++ src/main/java/pl/mjedynak/idea/plugins/builder/action/GoToBuilderAction.java
@@ -2,15 +2,11 @@ package pl.mjedynak.idea.plugins.builder.action;
 
 import pl.mjedynak.idea.plugins.builder.action.handler.AbstractBuilderActionHandler;
 import pl.mjedynak.idea.plugins.builder.action.handler.GoToBuilderActionHandler;
-import pl.mjedynak.idea.plugins.builder.factory.GoToBuilderPopupListFactory;
-import pl.mjedynak.idea.plugins.builder.gui.displayer.GoToBuilderPopupDisplayer;
 
 public class GoToBuilderAction extends AbstractBuilderAction {
 
     static {
         picoContainer.registerComponentImplementation(GoToBuilderActionHandler.class);
-        picoContainer.registerComponentImplementation(GoToBuilderPopupDisplayer.class);
-        picoContainer.registerComponentImplementation(GoToBuilderPopupListFactory.class);
         builderActionHandler =
                 (AbstractBuilderActionHandler) picoContainer.getComponentInstanceOfType(GoToBuilderActionHandler.class);
     }
