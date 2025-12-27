f61162f7be028fc6e79f51cb69dfc7f71859c9c8
Rey <89825425+kingg22@users.noreply.github.com>
2025-12-27 00:48:55 -0500

    fix: remove all unnecessary register components from AbstractBuilderAction

--- src/main/java/pl/mjedynak/idea/plugins/builder/action/AbstractBuilderAction.java
+++ src/main/java/pl/mjedynak/idea/plugins/builder/action/AbstractBuilderAction.java
@@ -4,21 +4,12 @@ import com.intellij.openapi.editor.actionSystem.EditorAction;
 import com.intellij.util.pico.DefaultPicoContainer;
 import org.picocontainer.MutablePicoContainer;
 import pl.mjedynak.idea.plugins.builder.action.handler.AbstractBuilderActionHandler;
-import pl.mjedynak.idea.plugins.builder.action.handler.DisplayChoosers;
-import pl.mjedynak.idea.plugins.builder.psi.BuilderPsiClassBuilder;
-import pl.mjedynak.idea.plugins.builder.writer.BuilderWriter;
 
 public abstract class AbstractBuilderAction extends EditorAction {
 
     protected static AbstractBuilderActionHandler builderActionHandler;
 
     protected static MutablePicoContainer picoContainer = new DefaultPicoContainer();
-
-    static {
-        picoContainer.registerComponentImplementation(BuilderPsiClassBuilder.class);
-        picoContainer.registerComponentImplementation(BuilderWriter.class);
-        picoContainer.registerComponentImplementation(DisplayChoosers.class);
-    }
 
     protected AbstractBuilderAction() {
         super(builderActionHandler);
