f3de8a98655ba864a2ea361ba93a3d46bac65923
Rey <89825425+kingg22@users.noreply.github.com>
2025-12-27 23:41:15 -0500

    build(gradle): remove spotless, errorprone nullaway, add kotlin and configure
    This is the new fork with my group/name
    Include mockito kotlin because mockito needs helpers to understand kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,32 @@
-import net.ltgt.gradle.errorprone.errorprone
-import net.ltgt.gradle.nullaway.nullaway
 import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
+import org.jetbrains.kotlin.gradle.dsl.JvmDefaultMode
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 plugins {
-    java
     id("org.jetbrains.intellij.platform")
-    id("com.diffplug.spotless") version "8.1.0"
-    id("net.ltgt.errorprone") version "4.3.0"
-    id("net.ltgt.nullaway") version "2.3.0"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktlint)
 }
 
-group = "pl.mjedynak"
-version = "1.5.0-SNAPSHOT"
+group = "io.github.kingg22"
+version = "0.0.1-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    compilerOptions {
+        apiVersion.set(KotlinVersion.KOTLIN_2_3)
+        languageVersion.set(apiVersion)
+        jvmTarget.set(JvmTarget.JVM_17)
+        jvmDefault.set(JvmDefaultMode.NO_COMPATIBILITY)
+        extraWarnings.set(true)
+        allWarningsAsErrors.set(true)
+        explicitApi()
+    }
 }
 
 dependencies {
@@ -25,11 +36,10 @@ dependencies {
         }
         bundledPlugin("com.intellij.java")
     }
-    errorprone("com.google.errorprone:error_prone_core:2.45.0")
-    errorprone("com.uber.nullaway:nullaway:0.12.15")
     testImplementation(platform("org.junit:junit-bom:5.14.1"))
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("org.mockito:mockito-junit-jupiter:5.10.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:6.1.0")
     testImplementation("org.assertj:assertj-core:3.25.3")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -75,36 +85,6 @@ tasks.test {
     useJUnitPlatform()
 }
 
-spotless {
-    java {
-        palantirJavaFormat()
-    }
-}
-
-nullaway {
-    annotatedPackages.add("pl.mjedynak.idea.plugins.builder")
-    onlyNullMarked.set(false)
-}
-
-tasks.withType<JavaCompile> {
-    options.errorprone {
-        isEnabled.set(true)
-        allSuggestionsAsWarnings.set(true)
-        nullaway {
-            error()
-            suggestSuppressions.set(true)
-            checkContracts.set(true)
-            isExhaustiveOverride.set(true)
-            knownInitializers.set(
-                listOf(
-                    "com.intellij.openapi.options.UnnamedConfigurable.createComponent",
-                    "pl.mjedynak.idea.plugins.builder.gui.CreateBuilderDialog.show",
-                    "pl.mjedynak.idea.plugins.builder.action.handler.AbstractBuilderActionHandler.doExecute",
-                ),
-            )
-        }
-    }
-}
-tasks.compileTestJava {
-    options.errorprone.isEnabled.set(false)
+ktlint {
+    version.set(libs.versions.ktlint.pinterest)
 }
